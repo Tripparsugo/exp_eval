d4495e15db53409f346d0fd4133e88d708627cb4
Tripparsugo <federico.lagrasta@gmail.com>
2020-12-04 22:59:24 +0100

    c3

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,39 +2,37 @@ class App {
     words;
     DOMid;
     entrypoint;
-    count = 0;
     startTime;
-    listen;
+    wordsToVar;
+
 
     constructor(words, DOMid, wordsToVar) {
         this.words = words;
         this.DOMid = DOMid;
         this.entrypoint = document.getElementById(this.DOMid);
-        this.callback = callback;
-        this.listen = true;
+        this.wordsToVar = wordsToVar;
     }
 
     start() {
-        // let ejs = require('ejs');
+        const formID = "hello";
 
         this.startTime = new Date().getMilliseconds();
+        // const template = {formId:formID, title:variables, variables: this.wordsToVar(variables)};
+        const template = {formId:formID, title:variables};
+        this.entrypoint.innerHTML = ejs.views_test2(template);
+        const form = document.getElementById(formID);
 
-         let html = ejs.render(caseTest01());
-         console.log(entrypoint.innerHTML);
-        // let html = '<button id="btn" type="button">Click Me!</button>'
-        entrypoint.innerHTML = html;
-        let btn = document.getElementById("btn");
-        btn.onclick = (e)=>this.doStuff(e);
+        return new Promise(resolve => {
+            form.onsubmit = (e)=>{
+                e.preventDefault();
+                resolve(e);
+            }
+        });
     }
 
-    doStuff(e){
-        e.preventDefault();
-        if(this.listen) {
-            this.count++;
-            this.callback(e);
-            console.log(this.count);
-        }
-    }
+
+
+
 
 
 
