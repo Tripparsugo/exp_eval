15ef44d54058bb7d02a588f21bd5e79a81a82a24
Tripparsugo <federico.lagrasta@gmail.com>
2020-12-04 19:13:36 +0100

    c2

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,19 +3,24 @@ class App {
     DOMid;
     entrypoint;
     count = 0;
+    startTime;
+    listen;
 
-    constructor(words, DOMid, callback) {
+    constructor(words, DOMid, wordsToVar) {
         this.words = words;
         this.DOMid = DOMid;
         this.entrypoint = document.getElementById(this.DOMid);
         this.callback = callback;
+        this.listen = true;
     }
 
     start() {
         // let ejs = require('ejs');
 
-         let html = entrypoint.innerHTML = ejs.render(caseTest01());
-         console.log(entrypoint.innerHTML)
+        this.startTime = new Date().getMilliseconds();
+
+         let html = ejs.render(caseTest01());
+         console.log(entrypoint.innerHTML);
         // let html = '<button id="btn" type="button">Click Me!</button>'
         entrypoint.innerHTML = html;
         let btn = document.getElementById("btn");
@@ -23,19 +28,15 @@ class App {
     }
 
     doStuff(e){
-        this.count++;
-        console.log(this.count);
-
-
+        e.preventDefault();
+        if(this.listen) {
+            this.count++;
+            this.callback(e);
+            console.log(this.count);
+        }
     }
 
-    toCamel(words){
-       return words.map(w=>w.charAt(0).toUpperCase() + w.substr(1,w.length)).join("");
-    }
 
-    toKebab(words){
-        words.join("-");
-    }
 
 
 }
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,42 @@
 console.log("hellow")
 
 
+variables = ['a', 'b'];
+c;
+results;
+
+const entrypoint = "entrypoint";
+
+new App({}, entrypoint).start();
 
 
-new App({}, "entrypoint").start();
+
+async function getResults() {
+    for(let v in variables){
+       results[c] =  await new App(v, entrypoint).start();
+       c++;
+    }
+
+    //fetch
+}
+
+
+function createTest(e) {
+    results[c++] = e.toJSON();
+    if(c >=  variables.length){
+        //do a fetch
+    } else {
+        new App(variables[c], entrypoint).start();
+
+    }
+
+    function toCamel(words){
+        return words.map(w=>w.charAt(0).toUpperCase() + w.substr(1,w.length)).join("");
+    }
+
+    function toKebab(words){
+        words.join("-");
+    }
+
+
+}
