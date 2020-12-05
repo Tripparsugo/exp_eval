c5847d526f9877c466d3036c68d417c17b8edee7
Ted Le <tedle1997@yahoo.com>
2020-12-05 22:29:30 +0100

    commit list of words

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,7 +4,43 @@ console.log("hellow@@@@@@")
 let variables = [];
 variables[0] = [{words:["hello", "there", "general", "kenobi"], correct:true}, {words:["hellow", "there", "general", "kenobi"], correct:false}];
 variables[1] =  [{words:["hello", "world"], correct:true}, {words:["hellowwwww", "world"], correct:false}];
+variables[2] = [{words:["fresh", "freshwater", "fish"], correct:true},
+    {words:["fresh", "friedwater", "fish"], correct:false},
+    {words:["free ", "freshwater", "fish"], correct:false},
+    {words:["fresh", "frenzywater", "fish"], correct:false},
+]
 
+variables[3] = [{words:["tropical", "cat"], correct:true},
+    {words:["tripping", "cat"], correct:false},
+    {words:["tropical", "cut"], correct:false},
+    {words:["typical", "cat"], correct:false},
+]
+
+variables[4] = [{words:["data", "value"], correct:true},
+    {words:["data", "varied"], correct:false},
+    {words:["date", "value"], correct:false},
+    {words:["data", "vale"], correct:false},
+]
+
+variables[5] = [{words:["file", "name"], correct:true},
+    {words:["first", "name"], correct:false},
+    {words:["file", "numb"], correct:false},
+    {words:["filet", "name"], correct:false},
+]
+
+variables[6] = [{words:["internal", "frameInternal", "frame", "title", "pane",
+        "internal", "frame", "title", "panel", "Maximize",
+        "button", "window", "not", "focused", "state"], correct:true},
+    {words:["internal", "frameInternal", "frame", "title", "pane",
+            "intestinal", "frame", "title", "panel", "Maximize",
+            "button", "window", "not", "focused", "state"], correct:false},
+    {words:["internal", "frameInternal", "frame", "title", "pane",
+            "internal", "frame" ,"title", "panel", "Maximize",
+            "buttocks", "window", "not", "focused", "state"], correct:false},
+    {words:["internal", "frameInternal", "frame", "tilted", "pane",
+            "internal", "frame", "title", "panel", "Maximize",
+            "buttons", "window", "not", "focused", "state"], correct:false},
+]
 
 
 
